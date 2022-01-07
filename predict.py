211caa91f5f05950074737b0fa36a98e670e174c
hossamalaa69 <hossamalaa69.ha@gmail.com>
2022-01-07 20:53:26 +0200

    Add exception handling in the prediction code

--- predict.py
+++ predict.py
@@ -13,22 +13,29 @@ def main():
     
     # iterate over all images in order
     for i in range(len(data)):
-        # start the timer
-        start_time = time.time()
-        # 1-apply binarization and text cropping 
-        img = pre_processing(data[i])
-        # 2-get image features
-        feature = getFeatures(img).reshape(1, -1)
-        # 3-predict image class
-        y_class = model.predict(feature) + 1
-        # stop the timer
-        end_time = time.time()
-        delta_time = str(round((end_time-start_time), 2))
-        if delta_time == "0.00":
-            delta_time = "0.01"
-        # print results of current image
-        f_results.write(str(y_class[0]))
-        f_time.write(delta_time)
+        try:
+            # start the timer
+            start_time = time.time()
+            # 1-apply binarization and text cropping 
+            img = pre_processing(data[i])
+            # 2-get image features
+            feature = getFeatures(img).reshape(1, -1)
+            # 3-predict image class
+            y_class = model.predict(feature) + 1
+            # stop the timer
+            end_time = time.time()
+            delta_time = str(round((end_time-start_time), 2))
+            if delta_time == "0.00":
+                delta_time = "0.01"
+            # print results of current image
+            f_results.write(str(y_class[0]))
+            f_time.write(delta_time)
+        
+        except:
+            f_results.write("-1")
+            f_time.write("0.01") 
+            print("An exception occured at i =", i)       
+
         if i+1 != len(data):
             f_results.write("\n")
             f_time.write("\n")
--- readme.txt
+++ readme.txt
@@ -6,3 +6,6 @@ scikit-image        0.19.1
 scikit-learn        1.0.1
 scipy               1.7.1
 pickleshare         0.7.5
+
+to run the code, write the following command:
+python predict.py "path of the test pictures" "path of output folder"
